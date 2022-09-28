abe442f700e4ed5e937849b672613def22cc9a6a
timPrachasri <nantawat.prac@gmail.com>
2022-09-29 03:27:27 +0700

    feat: update tailwind + daisyui related configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,37 @@ module.exports = {
       fontFamily: {
         inter: ['Inter', ...defaultTheme.fontFamily.sans],
       },
+      height: {
+        halfscreen: '50vh',
+        '2/3screen': '67vh',
+      },
+      minHeight: {
+        halfscreen: '50vh',
+        halffull: '50%',
+        '2/3screen': '67vh',
+      },
+      maxHeight: {
+        halfscreen: '50vh',
+        halffull: '50%',
+        '2/3screen': '67vh',
+      },
+      zIndex: {
+        60: '60',
+        70: '70',
+        80: '80',
+        90: '90',
+        99: '99',
+      },
+      screens: {
+        tablet: '640px',
+        // => @media (min-width: 640px) { ... }
+
+        laptop: '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        desktop: '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
     },
   },
   plugins: [require('daisyui')],
