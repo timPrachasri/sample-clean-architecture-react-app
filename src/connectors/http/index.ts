fdf44415d3d82dbd087e354ebfc6bda9b1009b15
timPrachasri <nantawat.prac@gmail.com>
2022-09-29 03:29:19 +0700

    refactor: update http connector to be a single instance across the application

--- src/connectors/http/index.ts
+++ src/connectors/http/index.ts
@@ -1,9 +1,5 @@
 import axios from 'axios'
 
-export let httpService = axios.create()
-
-export const initHttpService = () => {
-  httpService = axios.create({
-    baseURL: process.env.WORK_TRIAL_API_URL,
-  })
-}
+export const httpService = axios.create({
+  baseURL: process.env.WORK_TRIAL_API_URL,
+})
